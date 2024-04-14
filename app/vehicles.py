3c6b31e1685152d874affe7f549e0727232b7ce3
Luis Pelaez <119474028+ruisu666@users.noreply.github.com>
2024-04-14 13:08:53 +0800

    added dropdown list qr, vehicles table, refined logs table

--- app/vehicles.py
+++ app/vehicles.py
@@ -1,24 +1,31 @@
 import mysql.connector
-from flask import flash, render_template, Blueprint
+from flask import flash, render_template, Blueprint, session, request, send_file
 from app.utils import close_db_connection, get_db_connection
-
 
 vehicles_bp = Blueprint('vehicles', __name__)
 
 @vehicles_bp.route('/myvehicles')
 def vehicles():
-    vehicles = get_all_vehicles()
+    if 'user_role' not in session:
+        session['user_role'] = 'user'  
+    
+    page = int(request.args.get('page', 1))  
+    user_id = session.get('user_id') 
+    
+    vehicles = get_vehicles_for_page(page, user_id)
+    
     if vehicles is None:
         flash("Failed to fetch vehicles.", "danger")
-        return render_template('vehicles.html', title='Vehicles', vehicles=[])
+        return render_template('vehicles.html', title='Vehicles', vehicles=[], page=page, user_role=session.get('user_role'))
     else:
-        return render_template('vehicles.html', title='Vehicles', vehicles=vehicles)
+        return render_template('vehicles.html', title='Vehicles', vehicles=vehicles, page=page, user_role=session.get('user_role'))
 
-def get_all_vehicles():
+def get_vehicles_for_page(page, user_id):
     try:
         connection = get_db_connection()
         cursor = connection.cursor(dictionary=True)
-        cursor.execute("SELECT * FROM vehicle")
+        offset = (page - 1) * 10 
+        cursor.execute("SELECT * FROM vehicle WHERE userID = %s LIMIT 10 OFFSET %s", (user_id, offset))
         vehicles = cursor.fetchall()
         return vehicles
     except mysql.connector.Error as err:
@@ -31,3 +38,5 @@ def get_all_vehicles():
         return None
     finally:
         close_db_connection(connection)
+
+
